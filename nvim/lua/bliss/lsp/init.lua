c96feb7f217a468da326e7a48ae829e8dc7ee4e5
sourabhxyz <swasti@sourabh.xyz>
2022-10-02 11:35:00 +0530

    added lsp signs

--- nvim/lua/bliss/lsp/init.lua
+++ nvim/lua/bliss/lsp/init.lua
@@ -11,3 +11,13 @@ require('lsp-setup').setup({
     }
   }
 })
+
+-- UI customization --
+-- See more information, here: https://github.com/neovim/nvim-lspconfig/wiki/UI-customization.
+
+-- Modifying the signs for sign column. By default it shows like `E` for `Error`.
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
