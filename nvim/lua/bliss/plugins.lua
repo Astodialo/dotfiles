2b101121677ec1543433ae7cc79e95f97587b57a
sourabhxyz <swasti@sourabh.xyz>
2022-10-20 18:55:09 +0530

    :retab

--- nvim/lua/bliss/plugins.lua
+++ nvim/lua/bliss/plugins.lua
@@ -3,16 +3,16 @@ local fn = vim.fn
 -- Automatically install packer if not already
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-	PACKER_BOOTSTRAP = fn.system({
-		"git",
-		"clone",
-		"--depth",
-		"1",
-		"https://github.com/wbthomason/packer.nvim",
-		install_path,
-	})
-	print("Installing packer close and reopen Neovim...")
-	vim.cmd([[packadd packer.nvim]])
+  PACKER_BOOTSTRAP = fn.system({
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  })
+  print("Installing packer close and reopen Neovim...")
+  vim.cmd([[packadd packer.nvim]])
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
@@ -26,46 +26,46 @@ vim.cmd([[
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-	return
+  return
 end
 
 -- Have packer use a popup window
 packer.init({
-	display = {
-		open_fn = function()
-			return require("packer.util").float({ border = "rounded" })
-		end,
-	},
+  display = {
+    open_fn = function()
+      return require("packer.util").float({ border = "rounded" })
+    end,
+  },
 })
 
 -- Install your plugins here
 return packer.startup(function(use)
-	use({ "wbthomason/packer.nvim" }) -- Have packer manage itself
-	use({ "nvim-lua/plenary.nvim" }) -- Useful lua functions used by lots of plugins
+  use({ "wbthomason/packer.nvim" }) -- Have packer manage itself
+  use({ "nvim-lua/plenary.nvim" }) -- Useful lua functions used by lots of plugins
 
-	-- Colorschemes
+  -- Colorschemes
   use { "catppuccin/nvim", as = "catppuccin" }
   use { "folke/tokyonight.nvim" }
   use "EdenEast/nightfox.nvim"
   use 'Mofiqul/dracula.nvim'
 
-	-- Telescope
-	use({ "nvim-telescope/telescope.nvim" })
+  -- Telescope
+  use({ "nvim-telescope/telescope.nvim" })
   use({ "nvim-lua/popup.nvim" })  -- putting it here as I am only using it for telescope's "telescope-media-files" extension
-	use({ "nvim-telescope/telescope-media-files.nvim" })
+  use({ "nvim-telescope/telescope-media-files.nvim" })
 
-	-- Treesitter
+  -- Treesitter
   -- It is used for better syntax highlighting
-	use({
-		"nvim-treesitter/nvim-treesitter",
+  use({
+    "nvim-treesitter/nvim-treesitter",
     run = ':TSUpdate'
-	})
+  })
   use {
-	  "windwp/nvim-autopairs",
+    "windwp/nvim-autopairs",
     config = function() require("nvim-autopairs").setup {} end
   }
   use {
-	  "windwp/nvim-ts-autotag",
+    "windwp/nvim-ts-autotag",
     config = function() require("nvim-ts-autotag").setup {} end
   }
   use({ "p00f/nvim-ts-rainbow" })  -- rainbow parentheses for neovim using tree-sitter
@@ -77,7 +77,7 @@ return packer.startup(function(use)
   use {
     'numToStr/Comment.nvim',
   }
-	use({ "JoosepAlviste/nvim-ts-context-commentstring" })  -- now in the case of embedded language like in case of JSX, we use this with our comment plugin above.
+  use({ "JoosepAlviste/nvim-ts-context-commentstring" })  -- now in the case of embedded language like in case of JSX, we use this with our comment plugin above.
 
   -- nvim-tree
   use {
@@ -130,7 +130,7 @@ return packer.startup(function(use)
   use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup =
     function()
       local g = vim.g
-			g.mkdp_page_title = "${name}.md"
+      g.mkdp_page_title = "${name}.md"
       g.mkdp_filetypes = { "markdown" }
       g.mkdp_browser = '/usr/bin/brave'
       g.mkdp_auto_close = 0
@@ -200,9 +200,9 @@ return packer.startup(function(use)
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
   }
-	-- Automatically set up your configuration after cloning packer.nvim
-	-- Put this at the end after all plugins
-	if PACKER_BOOTSTRAP then
-		require("packer").sync()
-	end
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
 end)
